5ab84f6aa507bd35716b6289edd5a9da1fa74420
yilmaz1998 <snn.ylmz9804@gmail.com>
2025-10-27 13:18:35 -0400

    feat(cocktails): fetch cocktails in frontend and fix database

--- next-prisma-2/src/app/page.tsx
+++ next-prisma-2/src/app/page.tsx
@@ -1,10 +1,42 @@
 "use client"
 
-import React from 'react'
+import { Cocktail } from "@/types/types"
+import { useState, useEffect } from "react"
+
 
 const Page = () => {
+  const [cocktails, setCocktails] = useState<Cocktail[]>([])
+
+  useEffect(() => {
+    const fetchCocktails = async () => {
+      try {
+        const response = await fetch("/api/cocktail")
+        const data = await response.json()
+        setCocktails(data)
+      } catch (error) {
+        throw new Error("Failed to fetch cocktails")
+      }
+    }
+
+    fetchCocktails()
+  }, [])
+
   return (
-    <div>page</div>
+    <div>
+      {cocktails.map((cocktail) => (
+        <div key={cocktail.id}>
+          <h2>{cocktail.name}</h2>
+          {cocktail.imageUrl && (
+            <img
+              src={cocktail.imageUrl}
+              alt={cocktail.name}
+              className="w-48 h-48 object-cover"
+            />
+          )}
+          <p>{cocktail.ingredients}</p>
+        </div>
+      ))}
+    </div>
   )
 }
 
---
+++ next-prisma-2/src/types/types.ts
@@ -0,0 +1,12 @@
+export interface User {
+    id: string;
+    username: string;
+    password: string;
+}
+
+export interface Cocktail {
+    id: string;
+    name: string;
+    imageUrl: string;
+    ingredients: string;
+}
