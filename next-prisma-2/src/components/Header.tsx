3e0ceaf3c82ae5c7caf7779740049e666313bf29
yilmaz1998 <snn.ylmz9804@gmail.com>
2025-10-29 13:04:36 -0400

    feat(sidebar): Add sidebar UI

--- next-prisma-2/src/components/Header.tsx
+++ next-prisma-2/src/components/Header.tsx
@@ -4,10 +4,11 @@ import { Button } from "@/components/ui/button";
 import { useTheme } from "next-themes";
 import { Moon, Sun } from "lucide-react";
 import { BiSolidDrink } from "react-icons/bi";
+import { FiMenu } from "react-icons/fi"
 import Link from "next/link"
 import { useSession, signOut } from "next-auth/react";
 import { Input } from "@/components/ui/input"
-import { useContext } from "react";
+import { useState, useContext } from "react";
 import { SearchContext } from "@/SearchContext";
 
 
@@ -20,6 +21,11 @@ const Header = () => {
     throw new Error("useSearch must be used within a SearchProvider");
   }  
   const { searchQuery, setSearchQuery } = context;
+  const [sideBarOpen, setSideBarOpen] = useState(false)
+
+  const toggleSidebar = () => {
+    setSideBarOpen(!sideBarOpen)
+}
 
   return (
     <div className="w-full px-4 py-4 fixed flex items-center bg-white dark:bg-black z-50 justify-between">
@@ -30,27 +36,43 @@ const Header = () => {
         </Link>
       </div>
 
-      <Input type="text" placeholder="Search..." value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
-      
-      <Button
+      <Input className="w-2/3" type="text" placeholder="Search..." value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
+
+      <Button onClick={toggleSidebar} >
+        <FiMenu className='text-2xl hover:text-gray-200' />
+      </Button> 
+
+      {sideBarOpen && (
+        <div
+          className="fixed inset-0 bg-black/40 z-30"
+          onClick={() => setSideBarOpen(false)}
+        />
+      )}
+
+      <div
+        className={`fixed top-0 right-0 h-full w-64 sm:w-72 md:w-84 bg-white dark:bg-black text-black dark:text-white transform ${sideBarOpen ? 'translate-x-0' : 'translate-x-full'
+          } transition-transform duration-500 z-40`}
+      >
+        <div className="mt-12 flex flex-col px-8 text-center space-y-5 text-2xl">
+        <Button
         variant="outline"
         onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-        className="flex items-center"
+        className="flex items-center mb-4"
       >
         {theme === "light" ? <Moon size={20} /> : <Sun size={20} />}
       </Button>
-
-      <div className="flex items-center gap-4">
-        {username ? (
-          <>
-            <span>Hello, {username}</span>
-            <Button onClick={() => signOut()}>Logout</Button>
-          </>
-        ) : (
-          <Link href="/auth/login">
-            <Button>Login</Button>
-          </Link>
-        )}
+          {username ? (
+            <>
+              <span className="mt-4 light:text-black ">Hello, {username}</span>
+              <Link href={"/favorites"}>My Favorites</Link>
+              <Button className="mt-2" onClick={() => signOut()}>Logout</Button>
+            </>
+          ) : (
+            <Link href="/auth/login">
+              <Button onClick={() => setSideBarOpen(false)} className="mt-4">Login</Button>
+            </Link>
+          )}
+        </div>
       </div>
     </div>
   )
